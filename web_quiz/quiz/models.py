19511493533f28ec50e712ca05acdf1698e83fce
neeraj1909 <neeraj1909@gmail.com>
2018-02-23 19:49:04 +0530

    added new model to store attempts

--- web_quiz/quiz/models.py
+++ web_quiz/quiz/models.py
@@ -1,4 +1,6 @@
+import random
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from model_utils.models import TimeStampedModel
 
@@ -8,6 +10,7 @@ class Question(TimeStampedModel):
 
     html = models.TextField(_('Question Text'))
     is_published = models.BooleanField(_('Has been published?'), default=False, null=False)
+    maximum_marks = models.DecimalField(_('Maximum Marks'), default=4, decimal_places=2, max_digits=6)
 
     def __str__(self):
         return self.html
@@ -20,6 +23,35 @@ class Choice(TimeStampedModel):
     is_correct = models.BooleanField(_('Is this answer correct?'), default=False, null=False)
     html = models.TextField(_('Choice Text'))
 
+    def __str__(self):
+        return self.html
 
-def __str__(self):
-    return self.html
+
+class QuizProfile(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_score = models.DecimalField(_('Total Score'), default=0, decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f'<QuizProfile: user={self.user}>'
+
+    def get_new_question(self):
+        used_questions_pk = AttemptedQuestion.objects.filter(quiz_profile=self).values_list('question__pk', flat=True)
+        remaining_questions = Question.objects.exclude(pk__in=used_questions_pk)
+        if not remaining_questions.exists():
+            return
+        return random.choice(remaining_questions)
+
+    def create_attempt(self, question):
+        attempted_question = AttemptedQuestion(question=question, quiz_profile=self)
+        attempted_question.save()
+
+
+class AttemptedQuestion(TimeStampedModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    quiz_profile = models.ForeignKey(QuizProfile, on_delete=models.CASCADE, related_name='attempts')
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE, null=True)
+    is_correct = models.BooleanField(_('Was this attempt correct?'), default=False, null=False)
+    marks_obtained = models.DecimalField(_('Marks Obtained'), default=0, decimal_places=2, max_digits=6)
+
+    def get_absolute_url(self):
+        return f'/submission-result/{self.pk}/'
